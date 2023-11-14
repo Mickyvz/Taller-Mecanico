beb51b5ddb2140c95db81feab4f5e474f24ab8cc
Miguel <vazquezmicky@gmail.com>
2023-11-14 12:24:55 -0600

    Mejoras del proyecto

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-let search = document.querySelector(".search-box");
-
-document.querySelector("#search-icon").onclick = () => {
-  search.classList.toggle("active");
-  // menu.classList.remove("active");
-};
-
-// let menu = document.querySelector(".navbar");
-
-// document.querySelector("#menu-icon").onclick = () => {
-//   menu.classList.toggle("active");
-//   search.classList.remove("active");
-// };
-
-// Esconder menu y scroll
-// window.onscroll = () => {
-//   menu.classList.remove("active");
-//   search.classList.remove("active");
-// };
-// // Header
 let header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
